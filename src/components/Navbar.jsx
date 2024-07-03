2c2286b6eace1b200ea13240a706895d2db6216d
patrick-231 <137486318+patrick-231@users.noreply.github.com>
2024-07-03 09:40:41 +0200

    Updated Github Url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -105,7 +105,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://github.com/patrick-231?tab=repositories"
+              href="https://github.com/patrick-231"
               target="_blank"
               rel="noopener noreferrer"
             >
